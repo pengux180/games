bae8ad2d40b634aec8305591942efa334b5459ce
pengux180 <pengux180@gmail.com>
2026-09-09 21:35:39 +0100

    Update games.ps1

--- games.ps1
+++ games.ps1
@@ -1,1 +1,5 @@
 taskkill /IM steam.exe /f
+wget -O ~/Downloads/game-setup.rar "https://raw.githubusercontent.com/pengux180/games/main/game-setup.rar"
+winget install 7zip.7zip --accept-package-agreements --accept-source-agreements
+& "C:\Program Files\7-Zip\7z.exe" x "$env:USERPROFILE\Downloads\game-setup.rar" -o"$env:USERPROFILE\Downloads" -y
+copy "$env:USERPROFILE\Downloads\game-setup\*" "C:\Program Files (x86)\Steam"
