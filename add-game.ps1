ec01bb7c9ec5058f7d1d2a4765745da18a46a520
pengux180 <pengux180@gmail.com>
2026-09-09 23:26:30 +0100

    Update add-game.ps1

--- add-game.ps1
+++ add-game.ps1
@@ -1,3 +1,4 @@
 wget -O ~/Downloads/paid-game.rar "https://raw.githubusercontent.com/pengux180/games/main/paid-game.rar"
 & "C:\Program Files\7-Zip\7z.exe" x "$env:USERPROFILE\Downloads\paid-game.rar" -o"$env:USERPROFILE\Downloads\paid-game" -y
-copy "$env:USERPROFILE\Downloads\paid-game\*" "C:\Program Files (x86)\Steam\config\lua"
+copy "$env:USERPROFILE\Downloads\paid-game\*.lua" "C:\Program Files (x86)\Steam\config\lua"
+copy "$env:USERPROFILE\Downloads\paid-game\*.manifest" "C:\Program Files (x86)\Steam\depotcache"
