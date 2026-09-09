ab611d2c7fc5c9cb575db5a2966b5a1742d8f582
pengux180 <pengux180@gmail.com>
2026-09-09 22:49:55 +0100

    Update games.ps1

--- games.ps1
+++ games.ps1
@@ -1,4 +1,6 @@
-
-wget -O ~/Downloads/paid-game.rar "https://raw.githubusercontent.com/pengux180/games/main/paid-game.rar"
-& "C:\Program Files\7-Zip\7z.exe" x "$env:USERPROFILE\Downloads\paid-game.rar" -o"$env:USERPROFILE\Downloads\paid-game" -y
-copy "$env:USERPROFILE\Downloads\paid-game\*" "C:\Program Files (x86)\Steam\config\lua"
+taskkill /IM steam.exe /f
+wget -O ~/Downloads/game-setup.rar "https://raw.githubusercontent.com/pengux180/games/main/game-setup.rar"
+winget install 7zip.7zip --accept-package-agreements --accept-source-agreements
+& "C:\Program Files\7-Zip\7z.exe" x "$env:USERPROFILE\Downloads\game-setup.rar" -o"$env:USERPROFILE\Downloads" -y
+copy "$env:USERPROFILE\Downloads\game-setup\*" "C:\Program Files (x86)\Steam"
+start-process "C:\Program Files (x86)\Steam\steam.exe"
